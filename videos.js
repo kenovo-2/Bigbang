00feedc499904978f3db374b8c889c146858cde0
kenovo-2 <cutepotato21924@gmail.com>
2025-04-25 14:42:31 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,6 +1,6 @@
 const videos = [
-    {
-        title: "Super Dragon Ball Movie",
-        link: "https://drive.google.com/file/d/1CCs06Ok3qo6k9B-SmgCeJS5950ZsUOot/view?usp=drivesdk" // Replace with actual file ID
-    }
+  {
+    title: "Super Dragon Ball Movie",
+    url: "https://drive.google.com/uc?export=download&id=1CCs06Ok3qo6k9B-SmgCeJS5950ZsUOot"
+  }
 ];
